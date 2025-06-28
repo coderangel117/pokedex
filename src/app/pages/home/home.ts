6bb55a0c838c2e1de639c858d37a2d8901d4c19e
Gabriel <perinogabriel1@gmail.com>
2025-06-28 23:02:18 +0200

    feat(app): add Home & Header component - Add first styles files - Install bootstrap & import it in files

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Header } from '../../layouts/header/header';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Header
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
